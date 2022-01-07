d33ddf6f408b47743fa8fae5ce628fb19ea37806
dlasso <dlasso@bigdataca.com>
2022-01-07 02:39:09 -0500

    finished apis get and post patients

--- Security/endpoints/profile_endpoints.py
+++ Security/endpoints/profile_endpoints.py
@@ -1,5 +1,4 @@
 # Rest Framewrok Django
-from rest_framework.decorators import action
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
